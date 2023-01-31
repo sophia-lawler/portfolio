cd29033a2662cedf1429f61648613a3e0e275464
sophia-lawler <sophialawler@gmail.com>
2023-01-31 19:47:45 +1300

    landing on about

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,10 +1,11 @@
 import Link from 'next/link'
+
 export default function Nav() {
   return (
     <div className="nav">
     <Link className="nav-item" href="/projects"> /Projects</Link>
     <Link className="nav-item" href="/cv"> /CV</Link>
-    <Link className="nav-item" href="/"> /Home</Link>
+    <Link className="nav-item" href="/"> /About</Link>
     </div>
   )
 }
