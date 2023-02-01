6d57dafdd6fce6b2ab1bc02a69836e5dea262f05
sophia-lawler <sophialawler@gmail.com>
2023-02-01 17:38:06 +1300

    to lowercase

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -3,9 +3,9 @@ import Link from 'next/link'
 export default function Nav() {
   return (
     <div className="nav">
-    <Link className="nav-item" href="/projects"> /Projects</Link>
-    <Link className="nav-item" href="/cv"> /CV</Link>
-    <Link className="nav-item" href="/"> /About</Link>
+    <Link className="nav-item" href="/projects"> /projects</Link>
+    <Link className="nav-item" href="/cv"> /cv</Link>
+    <Link className="nav-item" href="/"> /about</Link>
     </div>
   )
 }
