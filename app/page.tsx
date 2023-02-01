a76882d9c30eefc4595ed96f067550f48f1b3556
sophia-lawler <sophialawler@gmail.com>
2023-02-01 18:53:31 +1300

    about info add

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function About() {
   return (
     <main>
-      <Image src="/background.jpeg" alt={'distorted lilly photo'} width={1600} height={400}></Image>
+      <p className='about'> Kia ora, I&apos;m a junior software developer engaged in creative design, research and programming within Tāmaki Makaurau.   </p>
     </main>
   )
 }
