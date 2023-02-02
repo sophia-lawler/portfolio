2b73df0b32e8209aec96fd65d44938976138d2d3
sophia-lawler <sophialawler@gmail.com>
2023-02-02 17:31:26 +1300

    import model to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
-import Image from 'next/image'
+'use client'
+import ModelEnviro from '../components/Environment'
 
 export default function About() {
   return (
     <main>
-      <p className='about'> Kia ora, I&apos;m a junior software developer engaged in creative design, research and programming within Tāmaki Makaurau.   </p>
+      <ModelEnviro/>
     </main>
   )
 }
