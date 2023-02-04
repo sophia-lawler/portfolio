43ff72a8f07cc29a98a5436ee6738d7e25177259
sophia-lawler <sophialawler@gmail.com>
2023-02-04 16:19:31 +1300

    add home to nav

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,11 +1,13 @@
 import Link from 'next/link'
 
 export default function Nav() {
+ 
   return (
     <div className="nav">
-      {['projects', 'cv', 'about'].map(nav =>  <Link key={nav} className="nav-item" href={`${nav}`} > /{nav}</Link>
+      {['projects', 'cv', 'about'].map(nav =>  <Link key={nav} className="nav-item" href={`${nav}`} >/{nav}</Link>
 
       )}
+      <Link className="nav-item" href={'/'} >/</Link>
     </div>
   )
 }
